3337398f2fb16551e7b19c2510f45f41853e7b28
Dhawal Bhanushali <dhawalcbhanushali@gmail.com>
2017-11-08 11:40:41 +0530

    Uncomment transition code

--- app/controllers/rentals/add.js
+++ app/controllers/rentals/add.js
@@ -3,10 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         addRental(record) {
-            if (record.get('errors').has('title')) {
-                alert('Please update your email before attempting to save.');
-            }
-            // record.save().then(() => this.transitionToRoute('rentals'));
+            record.save().then(() => this.transitionToRoute('rentals'));
         }
     }
 });
